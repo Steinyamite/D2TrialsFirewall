624f188ebda1da6e27ab6af7dc410fb0a571bc55
Steinyamite <59343818+Steinyamite@users.noreply.github.com>
2021-06-02 19:06:25 -0400

    Update d2firewall.sh

--- d2firewall.sh
+++ d2firewall.sh
@@ -281,39 +281,63 @@ setup () {
   done
 
 # Blocking Servers
-sudo iptables -A INPUT -s 146.66.155.52 -j DROP
-sudo iptables -A INPUT -s 155.133.248.0/24 -j DROP
-sudo iptables -A INPUT -s 155.133.230.0/24 -j DROP
-sudo iptables -A INPUT -s 185.25.182.0/24 -j DROP
-sudo iptables -A INPUT -s 103.10.124.0/24 -j DROP
-sudo iptables -A INPUT -s 103.10.125.0/24 -j DROP
-sudo iptables -A INPUT -s 139.45.193.0/24 -j DROP
-sudo iptables -A INPUT -s 146.66.154.0/24 -j DROP
-sudo iptables -A INPUT -s 155.133.232.0/24 -j DROP
-sudo iptables -A INPUT -s 155.133.233.0/24 -j DROP
-sudo iptables -A INPUT -s 155.133.235.0/24 -j DROP
 
-sudo iptables -A INPUT -s 155.133.238.0/24 -j DROP
-sudo iptables -A INPUT -s 155.133.239.0/24 -j DROP
-sudo iptables -A INPUT -s 155.133.245.0/24 -j DROP
-sudo iptables -A INPUT -s 155.133.246.0/24 -j DROP
-sudo iptables -A INPUT -s 155.133.249.0/24 -j DROP
-sudo iptables -A INPUT -s 155.133.250.0/24 -j DROP
-sudo iptables -A INPUT -s 155.133.252.0/24 -j DROP
-sudo iptables -A INPUT -s 155.133.253.0/24 -j DROP
-sudo iptables -A INPUT -s 155.133.254.0/24 -j DROP
-
-sudo iptables -A INPUT -s 162.254.192.0/24 -j DROP
-sudo iptables -A INPUT -s 162.254.193.0/24 -j DROP
-sudo iptables -A INPUT -s 162.254.195.0/24 -j DROP
-sudo iptables -A INPUT -s 162.254.198.0/24 -j DROP
-sudo iptables -A INPUT -s 162.254.199.0/24 -j DROP
-sudo iptables -A INPUT -s 185.25.183.0/24 -j DROP
-sudo iptables -A INPUT -s 190.217.33.0/24 -j DROP
-sudo iptables -A INPUT -s 205.185.194.0/24 -j DROP
-sudo iptables -A INPUT -s 205.196.6.0/24 -j DROP
-sudo iptables -A INPUT -s 205.209.16.0/24 -j DROP
-sudo iptables -A INPUT -s 205.234.119.0/24 -j DROP
+sudo iptables -I FORWARD -d 103.10.124.0/24 -j DROP
+sudo iptables -I FORWARD -d 103.10.125.0/24 -j DROP
+sudo iptables -I FORWARD -d 103.28.54.0/24 -j DROP
+sudo iptables -I FORWARD -d 103.28.55.0/24 -j DROP
+sudo iptables -I FORWARD -d 146.66.154.0/24 -j DROP
+sudo iptables -I FORWARD -d 146.66.155.0/24 -j DROP
+sudo iptables -I FORWARD -d 146.66.156.0/23 -j DROP
+sudo iptables -I FORWARD -d 153.254.86.0/24 -j DROP
+sudo iptables -I FORWARD -d 155.133.224.0/24 -j DROP
+sudo iptables -I FORWARD -d 155.133.225.0/24 -j DROP
+#sudo iptables -I FORWARD -d 155.133.226.0/24 -j DROP
+sudo iptables -I FORWARD -d 155.133.227.0/24 -j DROP
+sudo iptables -I FORWARD -d 155.133.228.0/23 -j DROP
+sudo iptables -I FORWARD -d 155.133.230.0/23 -j DROP
+sudo iptables -I FORWARD -d 155.133.232.0/24 -j DROP
+sudo iptables -I FORWARD -d 155.133.233.0/24 -j DROP
+sudo iptables -I FORWARD -d 155.133.234.0/24 -j DROP
+sudo iptables -I FORWARD -d 155.133.238.0/24 -j DROP
+sudo iptables -I FORWARD -d 155.133.239.0/24 -j DROP
+sudo iptables -I FORWARD -d 155.133.242.0/23 -j DROP
+sudo iptables -I FORWARD -d 155.133.244.0/24 -j DROP
+sudo iptables -I FORWARD -d 155.133.245.0/24 -j DROP
+sudo iptables -I FORWARD -d 155.133.246.0/23 -j DROP
+sudo iptables -I FORWARD -d 155.133.248.0/24 -j DROP
+sudo iptables -I FORWARD -d 155.133.249.0/24 -j DROP
+sudo iptables -I FORWARD -d 155.133.250.0/24 -j DROP
+sudo iptables -I FORWARD -d 155.133.251.0/24 -j DROP
+sudo iptables -I FORWARD -d 155.133.252.0/24 -j DROP
+sudo iptables -I FORWARD -d 155.133.253.0/24 -j DROP
+sudo iptables -I FORWARD -d 155.133.254.0/24 -j DROP
+sudo iptables -I FORWARD -d 155.133.255.0/24 -j DROP
+sudo iptables -I FORWARD -d 162.254.192.0/24 -j DROP
+sudo iptables -I FORWARD -d 162.254.193.0/24 -j DROP
+sudo iptables -I FORWARD -d 162.254.194.0/23 -j DROP
+sudo iptables -I FORWARD -d 162.254.196.0/24 -j DROP
+sudo iptables -I FORWARD -d 162.254.197.0/24 -j DROP
+sudo iptables -I FORWARD -d 162.254.198.0/24 -j DROP
+sudo iptables -I FORWARD -d 162.254.199.0/24 -j DROP
+sudo iptables -I FORWARD -d 185.25.180.0/23 -j DROP
+sudo iptables -I FORWARD -d 185.25.182.0/24 -j DROP
+sudo iptables -I FORWARD -d 185.25.183.0/24 -j DROP
+sudo iptables -I FORWARD -d 190.216.121.0/24 -j DROP
+sudo iptables -I FORWARD -d 190.217.33.0/24 -j DROP
+sudo iptables -I FORWARD -d 192.69.98.0/24 -j DROP
+sudo iptables -I FORWARD -d 192.69.99.0/24 -j DROP
+sudo iptables -I FORWARD -d 205.185.194.0/24 -j DROP
+sudo iptables -I FORWARD -d 205.196.6.0/24 -j DROP
+sudo iptables -I FORWARD -d 208.64.200.0/24 -j DROP
+sudo iptables -I FORWARD -d 208.64.201.0/24 -j DROP
+sudo iptables -I FORWARD -d 208.64.202.0/24 -j DROP
+sudo iptables -I FORWARD -d 208.64.203.0/24 -j DROP
+sudo iptables -I FORWARD -d 208.78.164.0/24 -j DROP
+sudo iptables -I FORWARD -d 208.78.165.0/24 -j DROP
+sudo iptables -I FORWARD -d 208.78.166.0/24 -j DROP
+sudo iptables -I FORWARD -d 45.121.184.0/23 -j DROP
+sudo iptables -I FORWARD -d 45.121.186.0/23 -j DROP
 
   if [ -z "$1" ]; then
     echo -e "${GREEN}Setup is complete and Matchmaking Firewall is now active.${NC}"
